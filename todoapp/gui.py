cf57fee7578209817cd5f7c490ab780a44b487ed
Heiko Heinsalu <heiko.heinsalu@gmail.com>
2025-03-17 20:59:15 +0200

    Implement Complete and Exit buttons

--- todoapp/gui.py
+++ todoapp/gui.py
@@ -8,9 +8,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", 
                     enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do app", 
-                    layout=[[label], [input_box, add_button], [list_box, edit_button ] ], 
+                    layout=[[label], 
+                    [input_box, add_button], 
+                    [list_box, edit_button, complete_button ],
+                    [exit_button] ], 
                     font=("Helvetica", 20))
 
 
@@ -36,6 +41,18 @@ while True:
             todos[index] = new_todo             # Asendab uue väärtusega
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+            
+        
+        case "Complete":
+            todos_to_complete = values["todos"][0]  # Võtab valitud elemendi
+            todos = functions.get_todos()
+            todos.remove(todos_to_remove)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        
+        case "Exit":
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'][0])
