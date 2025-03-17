5a923584564c13c0c9162e7bcdd7afb35c9565d9
Heiko Heinsalu <heiko.heinsalu@gmail.com>
2025-03-17 21:28:50 +0200

    Change theme, add clock, complete and edit error

--- todoapp/gui.py
+++ todoapp/gui.py
@@ -1,7 +1,10 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("LightBrown13")
 
+clock = sg.Text("", key="clock")
 label = sg.Text("Type in a to-do: ")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
@@ -12,18 +15,18 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do app", 
-                    layout=[[label], 
-                    [input_box, add_button], 
-                    [list_box, edit_button, complete_button ],
-                    [exit_button] ], 
+                    layout=[[clock],
+                        [label], 
+                        [input_box, add_button], 
+                        [list_box, edit_button, complete_button ],
+                        [exit_button] ], 
                     font=("Helvetica", 20))
 
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(2, values["todos"])
+    event, values = window.read(timeout=800)
+    window["clock"].update(value=time.strftime("%d %B %Y %H:%M:%S"))
+    
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -33,24 +36,32 @@ while True:
             window["todos"].update(values=todos)
 
         case "Edit":
-            todos_to_edit = values["todos"][0]  # Võtab valitud elemendi
-            new_todo = values["todo"] + "\n"    # Võtab sisendiväljast uue väärtuse
+            try:
+                todos_to_edit = values["todos"][0]  # Võtab valitud elemendi
+                new_todo = values["todo"] + "\n"    # Võtab sisendiväljast uue väärtuse
 
-            todos = functions.get_todos()
-            index = todos.index(todos_to_edit)  # Leiab indeksi listis
-            todos[index] = new_todo             # Asendab uue väärtusega
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            
+                todos = functions.get_todos()
+                index = todos.index(todos_to_edit)  # Leiab indeksi listis
+                todos[index] = new_todo             # Asendab uue väärtusega
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+            except IndexError:
+                sg.popup("Please select a todo to edit")
+
+                
         
         case "Complete":
-            todos_to_complete = values["todos"][0]  # Võtab valitud elemendi
-            todos = functions.get_todos()
-            todos.remove(todos_to_remove)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")
-        
+            try:
+                todos_to_complete = values["todos"][0]  # Võtab valitud elemendi
+                todos = functions.get_todos()
+                todos.remove(todos_to_complete)
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+            except IndexError:
+                sg.popup("Please select a todo to complete")
+
+
         case "Exit":
             break
 
