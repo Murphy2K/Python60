95b6ea5a6a06423b91fbf6cd32eaf33c1bee8d44
Heiko Heinsalu <heiko.heinsalu@gmail.com>
2025-03-23 22:47:50 +0200

    Lisa add funktsioon web äppile

--- todoapp/web.py
+++ todoapp/web.py
@@ -1,7 +1,16 @@
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+
+
 
 st.title("My todo app")
 st.subheader("This is my todo app")
@@ -11,5 +20,4 @@ for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label="", placeholder="Add new todo...")
- 
+st.text_input(label=" ", placeholder="Add new todo...", on_change=add_todo, key="new_todo")
